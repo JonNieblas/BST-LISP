2a3316f8f450e24e8484898f841aeb809a08bc75
Jonathan Nieblas <jniebla1@msudenver.edu>
2019-05-05 16:34:38 -0600

    Implemented a simpler IOT, separating pieceds into individual definitions.

--- BST.rkt
+++ BST.rkt
@@ -1,68 +1,25 @@
 #lang racket
-(define (lower a x)
-  (if (null? x)
-    null
-    (if (< (car x) a)
-       (cons (car x) (lower a (cdr x)) )
-       (lower a (cdr x))
-    )
-  )
-)
+; help from: https://stackoverflow.com/questions/12850443/pre-in-and-post-order-of-an-arithmetic-tree-scheme-racket
 
-(define (app a x)
-  (if(null? x)
-     (cons a x)
-     (cons (car x) (app a (cdr x)))
-   )
-)
+; get root of tree
+(define (root-node tree)
+  (car tree))
 
-(define (ins a p x)
-  (if (= p 0)
-      (if (null? x)
-          (cons a x)
-          (cons a (ins a (- p 1) (cdr x)))
-      )
-      (if (null? x)
-          null
-          (cons (car x) (ins a (- p 1) (cdr x)))
+; get left side of tree
+(define (left-branch tree)
+  (cadr tree))
+
+; get right side of tree
+(define (right-branch tree)
+  (caddr tree))
+
+; go down left subtree first,
+; return to root when left tree returns null,
+; then go down right tree last
+(define (iot tree)
+  (if (null? tree)
+      '()
+      (append (iot (left-branch tree)) (list (root-node tree))(iot (right-branch tree))
       )
    )
-)
-
-; test: (iot '(3 (1 () ()) (7 (5 () ()) ()) ) )
-(define (iot bst)
-  (if (null? bst)
-      (println "null")
-      (if (null? (cdr bst))
-          null
-          (if (null? (car (cdr bst)))
-              (if (null? (cdr (cdr bst)))
-                  null
-                  (println (car bst))
-                  ;(cons (car bst) (iot (cdr (cdr bst))))
-                  )
-              (iot (car (cdr bst)))
-          )
-      )
-   )
-  (println "made it")
- )
-
-(define (iot2 bst)
-  (if (null? bst)
-      null
-      (check-both bst)
-   )
-)
-
-;function that checks both trees for null
-(define (check-both bst)
-  (if (null? (car (cdr bst)))
-      (cons (car bst) '())
-      (cons (check-both (car (cdr bst))) (car bst))
-   )
-  (if (null? (car (cdr (cdr bst))))
-      null
-      (cons (car bst) (check-both (car (cdr (cdr bst)))))
-  )
 )
